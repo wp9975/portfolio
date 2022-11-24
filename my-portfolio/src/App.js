31467b495de807d0454193fbb416bf737514b7c0
Wojciech Pieczonka <51296602+wp9975@users.noreply.github.com>
2022-11-24 21:26:12 +0100

    react init

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+ssfsffs
+    </div>
+  );
+}
+
+export default App;
---
+++ my-portfolio/src/components/navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from './navbar.module.css'
+
+const Navbar = () => {
+  return (
+    <div className={classes.navbar}>
+        
+    </div>
+  )
+}
+
+export default Navbar
